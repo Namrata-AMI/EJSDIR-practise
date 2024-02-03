0c4b641043a3e4758c91088c3ccc15d5d5c82413
Namrata <namratateshwer@gmail.com>
2024-02-03 17:55:54 +0530

    added some chages

--- index.js
+++ index.js
@@ -4,24 +4,22 @@ const path = require("path");
 const port = 8080;
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname,"/public/css")));
-app.use(express.static(path.join(__dirname,"/public/js")));
 
 app.set("view engine","ejs");
 
-app.set("views",path.join(__dirname,"/views"));
+app.set("views", path.join(__dirname,"/views"));
 
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 });
 
 app.get("/ig/:username",(req,res)=>{
-    let{username} = req.params;
+    let {username} = req.params;
     const instadata= require("./data.json");
    // console.log(username);//
-    console.log(instadata);
+  //  console.log(instadata);//
     const data = instadata[username];
-    console.log(data);
+    //console.log(data);//
     if(data){
     res.render("instagram.ejs",{data});  
     }
